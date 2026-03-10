d8c7b4937a7b0ebb489d0bde5452e5a1156f002c
sangsangbar34 <hoonsnageee34@jbnu.ac.kr>
2026-03-10 15:18:13 +0900

    homework02 제출

---
+++ homework02.py
@@ -0,0 +1,27 @@
+temp_c=30
+temp_f=temp_c*1.8+32
+print(f"{temp_c}℃ => {temp_f}F")
+
+temp_c=0
+temp_f=temp_c*1.8+32
+print(f"{temp_c}℃ => {temp_f}F")
+
+w=60
+h=1.7
+BMI=f"{w/(h*h)}"
+print(BMI)
+
+r=4
+O=f"{r*r}π"
+print(O)
+
+밑변=5
+윗변=3
+높이=4
+사다리꼴=f"{(밑변+윗변)*높이/2}"
+print(사다리꼴)
+
+물건=2000
+할인률=(1-0.15)
+할인값=f"{물건*할인률}원"
+print(할인값)
