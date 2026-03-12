085d3a4185fa7eb3408c33dc8c260821c0521933
sangsangbar34 <hoonsnageee34@jbnu.ac.kr>
2026-03-12 18:41:55 +0900

    과제수정

--- homework03.py
+++ homework03.py
@@ -1,21 +1,21 @@
-temp_c=int(input("온도를 입력하세요"))
+temp_c=int(input("온도℃를 입력하세요"))
 temp_f=(temp_c*9/5)+32
-print(f"{temp_c}℃ => {temp_f}F")
+print(f"{temp_c}℃ => {temp_f}℉")
 
-w=float(input("몸무게를 입력하세요"))
-h=float(input("키를 입력하세요"))
-BMI= f"{w / (h**2)}"
+w=float(input("몸무게(kg)를 입력하세요"))
+h=float(input("키(m)를 입력하세요"))
+BMI= f"BMI는 {w / (h**2)}입니다."
 print(BMI)
 
 import math
-r=int(input("반지름을 입력하세요"))
-area=f"{r**2*math.pi}"
+r=int(input("반지름(cm)을 입력하세요"))
+area=f"원의 넓이는 {r**2*math.pi}입니다."
 print(area)
 
-밑변=int(input("밑변을 입력하세요"))
-윗변=int(input("윗변을 입력하세요"))
-높이=int(input("높이를 입력하세요"))
-사다리꼴=(f"{(밑변+윗변)*높이/2}")
+밑변=int(input("밑변(cm)을 입력하세요"))
+윗변=int(input("윗변(cm)을 입력하세요"))
+높이=int(input("높이(cm)를 입력하세요"))
+사다리꼴=f"사다리꼴의 넓이는 {(밑변+윗변)*높이/2}입니다."
 print(사다리꼴)
 
 import math
@@ -24,7 +24,7 @@ x2=int(input("x2좌표를 입력하세요"))
 y1=int(input("y1좌표를 입력하세요"))
 y2=int(input("y2좌표를 입력하세요"))
 distance= math.sqrt((x2-x1)**2+(y2-y1)**2)
-print(distance)
+print(f"두점사이의 거리는 {distance} 입니다.")
 
 Hanrabong=int(input("한라봉 무게(g)를 입력하세요"))
 Hkcal=f"한라봉은 {Hanrabong/2}kcal 입니다."
